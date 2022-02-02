7f565b8d792c071269d6e17f9676b3344d086b2d
Halvaror <alvarolopezb94@gmail.com>
2022-02-03 00:18:17 +0100

    final

--- inter.c
+++ inter.c
@@ -6,7 +6,7 @@
 /*   By: alopezb <alopezb@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/01 16:31:28 by alopezb           #+#    #+#             */
-/*   Updated: 2022/02/02 18:43:39 by alopezb          ###   ########.fr       */
+/*   Updated: 2022/02/03 00:17:44 by alopezb          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,26 +18,32 @@ void checkstrings(const char *str1, const char *str2)
     int i;
     int j;
     int h;
+    int check;
 
     i = 0;
-    while (str1[i])
+    h = 0;
+    while (str1[i] != '\0')
     {
+        check = 0;
         j = 0;
         while (j < i)
         {
             if (str1[j] == str1[i])
-                i++;
+            {
+                check = 1;
+                break;
+            }   
             j++;
         }
         h = 0;
-        while (str2[h])
+        while (str2[h] && check != 1)
         {
             if (str1[i] == str2[h])
             {
                 write(1, &str1[i], 1);
                 break;
             }
-        h++;
+            h++;
         }
         i++;
     }
@@ -52,6 +58,6 @@ int main(int argc, char **argv)
         return (0);
     }
     checkstrings(argv[1], argv[2]);
-
+    write(1, "\n",1);
     return (0);
 }
